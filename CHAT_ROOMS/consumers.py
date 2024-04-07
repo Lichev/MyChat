d11fe6e2cdae748b6b2593b0905ce1884c56063b
Lichev <lichev96@gmail.com>
2024-04-07 10:10:48 +0300

    Handle empty spaces for WebSocket

--- CHAT_ROOMS/consumers.py
+++ CHAT_ROOMS/consumers.py
@@ -1,12 +1,13 @@
 import json
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from asgiref.sync import sync_to_async
+import re
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.room_group_name = self.get_group_name(self.room_name)
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -20,3 +21,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    def get_group_name(self, room_name):
+        sanitized_room_name = re.sub(r'\W+', '_', room_name)
+        return f'chat_{sanitized_room_name}'
--- MyChat/asgi.py
+++ MyChat/asgi.py
@@ -5,7 +5,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-import  CHAT_ROOMS.routing
+import CHAT_ROOMS.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyChat.settings')
