4af224d2364765e9cf90f6b601037ddf41b7b3ae
Lichev <lichev96@gmail.com>
2024-04-14 21:35:27 +0300

    friend handler in search

--- FRIEND/urls.py
+++ FRIEND/urls.py
@@ -4,9 +4,9 @@ from FRIEND.views import friendship_add_friend, friendship_accept, friendship_re
 
 urlpatterns = [
     path("add/<slug:to_username>/", friendship_add_friend, name='friendship_add_friend'),
-    path("accept/<int:friendship_request_id>/", friendship_accept, name='friendship_accept'),
-    path("reject/<int:friendship_request_id>/", friendship_reject, name='friendship_reject'),
-    path("cancel/<int:friendship_request_id>/", friendship_cancel, name='friendship_cancel'),
+    path("accept/<str:friendship_request_id>/", friendship_accept, name='friendship_accept'),
+    path("reject/<str:friendship_request_id>/", friendship_reject, name='friendship_reject'),
+    path("cancel/<str:friendship_request_id>/", friendship_cancel, name='friendship_cancel'),
     path("remove/<int:friend_id>/", remove_friend_view, name='remove_friend'),
     path("my-list/", show_friends_view, name='show_friends'),
     path('friend-requests/', show_friends_request, name='show_friends_request')
